f8355f96b548613f8319d1ea862ce5926a60ab72
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-27 00:01:43 +0600

    Python PDF

--- assignment 08/collage/views.py
+++ assignment 08/collage/views.py
@@ -5,7 +5,6 @@ from django.views import generic
 class CollageView(generic.View):
     def get(self, request):
         context = {
-            "collage_name": "AI Quest",
-            "collage_location": "Dhaka",
+            
         }
         return render(request, "collage/collage.html", context)
