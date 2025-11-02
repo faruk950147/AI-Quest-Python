0d0a0991191df7b8616dd1ed8945170c89864ee1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 08:19:26 +0600

    Python PDF

--- assignment 08/teacher/views.py
+++ assignment 08/teacher/views.py
@@ -4,9 +4,7 @@ from django.views import generic
 class TeacherView(generic.View):
     def get(self, request):
         context = {
-            "teacher_name": "John Doe",
-            "teacher_age": 20,
-            "teacher_salary": 5000,
+
         }
         return render(request, "teacher/teacher.html", context)
 
--- assignment 12/store/views.py
+++ assignment 12/store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+class HomeView(generic.View):
+    def get(self, request):
+        context = {
+
+        }
+        return render(request, "store/home.html", context)
