2ab544c387a4e4e882afa46ca048d4ada70665c5
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 17:35:30 +0600

    Python PDF

--- assignment 12/account/models.py
+++ assignment 12/account/models.py
@@ -16,8 +16,8 @@ DIVISION_CHOICES = (
     ('Sylhet', 'Sylhet'),
 )
 
-class Customer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customers')
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=200)
     division = models.CharField(choices=DIVISION_CHOICES, max_length=50)
     district = models.CharField(max_length=200)
@@ -25,7 +25,7 @@ class Customer(models.Model):
     villorroad = models.CharField(max_length=100)
     phone = models.CharField(max_length=15, default='+880')
     zipcode = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='Customer/%Y/%m/%d/')
+    image = models.ImageField(upload_to='Profile/%Y/%m/%d/')
     
     @property
     def image_tag(self):
@@ -36,7 +36,7 @@ class Customer(models.Model):
 
     class Meta:
         ordering = ['id']
-        verbose_name_plural = '01. Customers'
+    verbose_name_plural = '01. Profiles'
 
     def __str__(self):
         return f"{self.name} - {self.user.username}"
