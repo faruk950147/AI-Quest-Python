e1e8044f6e277baf6562c3c7bbb2673246339a75
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 16:31:18 +0600

    Python PDF

--- assignment 12/account/urls.py
+++ assignment 12/account/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path
-
+from account.views import (
+    SignInView,
+    SignUpView,
+    SignOutView,
+    PasswordChangeView,
+    PasswordResetView,
+    ProfileView,
+    
+)
 urlpatterns = [
     # Define your URL patterns here
+    path('sign-up/', SignUpView.as_view(), name='sign-up'),
+    path('sign-in/', SignInView.as_view(), name='sign-in'),
+    path('sign-out/', SignOutView.as_view(), name='sign-out'),
+    path('password-change/', PasswordChangeView.as_view(), name='password-change'),
+    path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- assignment 12/account/views.py
+++ assignment 12/account/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import generic
 # Create your views here.
+class SignUpView(generic.View):
+    def get(self, request):
+        
+        return render(request, 'account/sign-up.html', {})
+    
+class SignInView(generic.View):
+    def get(self, request):
+        return render(request, 'account/sign-in.html', {})
+
+class SignOutView(generic.View):
+    def get(self, request):
+        return redirect('sign-in')
+    
+class PasswordChangeView(generic.View):
+    def get(self, request):
+        return render(request, 'account/password-change.html', {})
+    
+class PasswordResetView(generic.View):
+    def get(self, request):
+        return render(request, 'account/password-reset.html', {})
+
+class ProfileView(generic.View):
+    def get(self, request):
+        return render(request, 'account/profile.html', {})
