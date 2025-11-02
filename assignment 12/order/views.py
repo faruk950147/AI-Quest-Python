b8c6320916e0446b7d682b8376f89c1bdf1d2206
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 18:42:40 +0600

    Python PDF

--- assignment 12/order/views.py
+++ assignment 12/order/views.py
@@ -5,6 +5,10 @@ from django.views import generic
 class CheckoutView(generic.View):
     def get(self, request):
         return render(request, 'order/checkout.html')
+    
+class OrderView(generic.View):
+    def get(self, request):
+        return render(request, 'order/order.html')
 
 class AddressView(generic.View):
     def get(self, request):
