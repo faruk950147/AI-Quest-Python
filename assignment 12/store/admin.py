d08af2ebc242c741c31c9fa1d7b5399e256e9316
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 20:26:22 +0600

    Python PDF

--- assignment 12/store/admin.py
+++ assignment 12/store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from store.models import (
+    Category,
+    Brand,
+    Product,
+)
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'parent', 'title', 'slug', 'keyword', 'description', 'image_tag', 'status', 'created_date', 'updated_date')
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(Category, CategoryAdmin)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'keyword', 'description', 'image_tag', 'status', 'created_date', 'updated_date')
+    prepopulated_fields = {'slug': ('title',)}  
+admin.site.register(Brand, BrandAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'brand', 'title', 'slug', 'old_price', 'sale_price', 'available_stock', 'discount_percent', 'keyword', 'description', 'image_tag', 'status', 'created_date', 'updated_date')
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(Product, ProductAdmin)
