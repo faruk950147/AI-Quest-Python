9d004888509fe31de5d6d74ed5e2f159b04ba2a4
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 22:27:00 +0600

    Python PDF

--- assignment 12/store/urls.py
+++ assignment 12/store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from store.views import (
-    HomeView, SingleProductView
+    HomeView, SingleProductView, CategoryProductView
 )
 urlpatterns = [
     # Define your URL patterns here
     path('', HomeView.as_view(), name='home'),
     path('single-product/', SingleProductView.as_view(), name='single-product'),
+    path('category-product/<int:id>', CategoryProductView.as_view(), name='category-product'),
 ]
--- assignment 12/store/views.py
+++ assignment 12/store/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from django.views import generic
-
+from store.models import Category, Brand, Product
 # Create your views here.
 class HomeView(generic.View):
     def get(self, request):
-        context = {
 
+        context = {
         }
         return render(request, "store/home.html", context)
-    
+
 class SingleProductView(generic.View):
     def get(self, request):
         context = {
 
         }
         return render(request, "store/single-product.html", context)
+    
+class CategoryProductView(generic.View):
+    def get(self, request, id):
+        # Product model category is ForeignKey (Category model) type
+        # it code Category table title field 'women' icontains filter use
+        # women = Product.objects.filter(category__title__icontains='women')
+        # print(women)
+        category = Product.objects.filter(category__id=id)
+        print(category)
+
+        context = {
+            'category': category,
+        }
+        return render(request, "store/category-product.html", context)
