17913941f7fe1077e27572f3e10fbc2a631c5e40
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 08:36:32 +0600

    Python PDF

--- assignment 12/account/models.py
+++ assignment 12/account/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+DIVISION_CHOICES = (
+    ('Dhaka', 'Dhaka'),
+    ('Rangpur', 'Rangpur'),
+    ('Rajshahi', 'Rajshahi'),
+    ('Khulna', 'Khulna'),
+    ('Barishal', 'Barishal'),
+    ('Chattogram', 'Chattogram'),
+    ('Mymensingh', 'Mymensingh'),
+    ('Sylhet', 'Sylhet'),
+)
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profiles')
+    name = models.CharField(max_length=200)
+    division = models.CharField(choices=DIVISION_CHOICES, max_length=50)
+    district = models.CharField(max_length=200)
+    thana = models.CharField(max_length=100)
+    villorroad = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15, default='+880')
+    zipcode = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name_plural = 'User Profiles'
+
+    def __str__(self):
+        return f"{self.name} - {self.user.username}"
