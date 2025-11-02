509d7919ff71b8cd57165a238e1ff0a9b066856c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-02 22:38:32 +0600

    Python PDF

--- assignment 12/store/views.py
+++ assignment 12/store/views.py
@@ -4,8 +4,14 @@ from store.models import Category, Brand, Product
 # Create your views here.
 class HomeView(generic.View):
     def get(self, request):
-
         context = {
+        """ Product model category is ForeignKey (Category model) type
+        it code Category table title field 'borkha' icontains filter use
+        borkha category product select and show """
+        
+        'borkha': Product.objects.filter(category__title__icontains='borkha')
+        
+
         }
         return render(request, "store/home.html", context)
 
@@ -18,10 +24,7 @@ class SingleProductView(generic.View):
     
 class CategoryProductView(generic.View):
     def get(self, request, id):
-        # Product model category is ForeignKey (Category model) type
-        # it code Category table title field 'women' icontains filter use
-        # women = Product.objects.filter(category__title__icontains='women')
-        # print(women)
+
         category = Product.objects.filter(category__id=id)
         print(category)
 
