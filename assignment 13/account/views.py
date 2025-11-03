12de83ea0f2ac9105dec4a5885c8223c23246db2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-03 16:50:34 +0600

    Python PDF

--- assignment 13/account/views.py
+++ assignment 13/account/views.py
@@ -9,7 +9,8 @@ class SignUpView(View):
     def get(self, request):
         if request.user.is_authenticated:
             return redirect('home')
-        return render(request, 'account/sign-up.html', {'form': SignUpForm()})
+        form = SignUpForm()
+        return render(request, 'account/sign-up.html', {'form': form})
 
     def post(self, request):
         form = SignUpForm(request.POST)
@@ -25,17 +26,23 @@ class SignInView(View):
     def get(self, request):
         if request.user.is_authenticated:
             return redirect('home')
-        return render(request, 'account/sign-in.html', {'form': SignInForm()})
+        form = SignInForm()
+        return render(request, 'account/sign-in.html', {'form': form})
 
     def post(self, request):
-        username = request.POST.get('username') 
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, f'Welcome back, {user.username}!')
-            return redirect('home')
+        form = SignInForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {user.username}!')
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password.')
         else:
-            messages.error(request, 'Invalid username or password.')
-            return render(request, 'account/sign-in.html', {'form': SignInForm()})
+            messages.error(request, 'Please correct the errors below.')
+
+        return render(request, 'account/sign-in.html', {'form': form})
+
