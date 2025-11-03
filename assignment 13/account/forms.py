153a076879ba55e91a7490c4acf55e8575686fe2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-03 17:59:32 +0600

    Python PDF

--- assignment 13/account/forms.py
+++ assignment 13/account/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import (
+    UserCreationForm, 
+    AuthenticationForm,
+    PasswordChangeForm, #it's with old password
+    SetPasswordForm  #it's without old password
+)
 
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
