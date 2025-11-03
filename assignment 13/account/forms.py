28038e61666e66427f38bc4bc734134c27250441
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-03 13:32:24 +0600

    Python PDF

--- assignment 13/account/forms.py
+++ assignment 13/account/forms.py
@@ -2,9 +2,29 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import (
     UserCreationForm
-)
-class SignUpForm(UserCreationForm):
+)# forms.py
+from django import forms
+from django.contrib.auth.models import User
 
+class SignUpForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists")
+        return email
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
