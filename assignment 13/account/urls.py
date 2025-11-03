7ae81c4bc04f560622dde242b7a7e5d164f275a6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-03 18:12:47 +0600

    Python PDF

--- assignment 13/account/urls.py
+++ assignment 13/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from account.views import SignUpView, SignInView
+from account.views import SignUpView, SignInView, ChangesPasswordView
 urlpatterns = [
     path('sign-up/', SignUpView.as_view(), name='sign-up'),
     path('sign-in/', SignInView.as_view(), name='sign-in'),
+    path('change-password/', ChangesPasswordView.as_view(), name='change-password'),
 ]
