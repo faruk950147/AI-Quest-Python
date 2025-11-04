5fa825970f5bf6ca982f2319da3bf562ce7e7688
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-04 18:58:06 +0600

    Python PDF

--- assignment 13/account/admin.py
+++ assignment 13/account/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff')
+    readonly_fields = ('username', 'email', 'first_name', 'password', 'last_name', 'is_staff', 'is_active')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+# unregister default User admin
 admin.site.unregister(User)
+# register custom User admin
 admin.site.register(User, UserAdmin)
