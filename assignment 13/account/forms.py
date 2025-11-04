790de9961eda812ff17b558a35b5a471e8ffec0d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-04 08:25:28 +0600

    Python PDF

--- assignment 13/account/forms.py
+++ assignment 13/account/forms.py
@@ -1,34 +1,57 @@
 from django import forms
 from django.contrib.auth.models import User
+# why use gettext_lazy as _ ? because it's for translation purpose
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import (
     UserCreationForm, 
     AuthenticationForm,
-    PasswordChangeForm, #it's with old password
-    SetPasswordForm  #it's without old password
+    PasswordChangeForm,  # it's with old password
+    SetPasswordForm      # it's without old password
 )
 
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         # Call the parent class __init__ method
         super(SignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter username'})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter password'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm password'})
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter email'}))
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Enter username')
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Enter password')
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Confirm password')
+        })
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control', 
+            'placeholder': _('Enter email')
+        })
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            'username': _('Username'),
+            'email': _('Email'),
+            'password1': _('Password'),
+            'password2': _('Confirm Password'),
+        }
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
         if User.objects.filter(username=username).exists():
-            raise forms.ValidationError("Username already exists")
+            raise forms.ValidationError(_("Username already exists"))
         return username
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Email already exists")
+            raise forms.ValidationError(_("Email already exists"))
         return email
 
     def save(self, commit=True):
@@ -38,25 +61,45 @@ class SignUpForm(UserCreationForm):
             user.save()
         return user
 
+
 class SignInForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         # Call the parent class __init__ method
         super(SignInForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter username'})
-        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter password'})
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Enter username')
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Enter password')
+        })
     
     class Meta:
         model = User
         fields = ['username', 'password']
+
 
 class ChangePasswordForm(PasswordChangeForm):
     # Extending the parent class PasswordChangeForm
     def __init__(self, user, *args, **kwargs):
         # Call the parent class __init__ method
         super(ChangePasswordForm, self).__init__(user, *args, **kwargs) 
-        self.fields['old_password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter old password', 'type': 'password'})
-        self.fields['new_password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter new password', 'type': 'password'})
-        self.fields['new_password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm new password', 'type': 'password'})
+        self.fields['old_password'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Enter old password'), 
+            'type': 'password'
+        })
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Enter new password'), 
+            'type': 'password'
+        })
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control', 
+            'placeholder': _('Confirm new password'), 
+            'type': 'password'
+        })
 
     class Meta:
         model = User
