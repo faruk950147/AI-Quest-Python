97273dfee0c429b176842633d6fb428ba5e65520
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-04 18:28:18 +0600

    Python PDF

--- assignment 12/accounts/forms.py
+++ assignment 12/accounts/forms.py
@@ -7,7 +7,8 @@ from django.contrib.auth.forms import (
     UserCreationForm, 
     AuthenticationForm,
     PasswordChangeForm,  # it's with old password
-    SetPasswordForm      # it's without old password
+    SetPasswordForm,   # it's without old password
+    PasswordResetForm
 )
 # SignUpForm
 class SignUpForm(UserCreationForm):
@@ -62,3 +63,19 @@ class ChangePasswordForm(PasswordChangeForm):
     class Meta:
         model = User
         fields = ('old_password', 'new_password1', 'new_password2')
+        
+# ResetPasswordForm
+class ResetPasswordForm(PasswordResetForm):
+    email = forms.EmailField(label=_('Email'), max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email', 'class':'form-control', 'placeholder':'Email'}))
+    class Meta:
+        model = User
+        fields = ('email',)
+        
+# SetNewPasswordForm
+class SetNewPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(label=_('New Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'class':'form-control'}))
+    new_password2 = forms.CharField(label=_('Confirm New Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'class':'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('new_password1', 'new_password2')
