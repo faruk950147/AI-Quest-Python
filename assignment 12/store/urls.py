e5ae4bb25df433770701c9079841f25bdb4b196c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-08 05:03:21 +0600

    I have added Rest API

--- assignment 12/store/urls.py
+++ assignment 12/store/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('single-product/<str:slug>/<int:id>/', SingleProductView.as_view(), name='single-product'),
     path('category-product/<str:slug>/<int:id>/', CategoryProductView.as_view(), name='category-product'),
-    path('category-product/<str:slug>/<int:id>/<str:data>/', CategoryProductView.as_view(), name='filters-products'),
+    path('category-product/<str:slug>/<int:id>/<str:data>/', CategoryProductView.as_view(), name='category-filter'),
     path('brand-product/<str:slug>/<int:id>/', BrandProductView.as_view(), name='brand-product'),
 ]
 
