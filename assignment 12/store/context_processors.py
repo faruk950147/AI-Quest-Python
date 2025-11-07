df931d2a074853910672ae2153448ee8cdec6e72
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-07 20:37:07 +0600

    I have added Rest API

--- assignment 12/store/context_processors.py
+++ assignment 12/store/context_processors.py
@@ -4,7 +4,6 @@ from store.models import (
 )
 def get_filters(request):
     categories = Category.objects.filter(status='ACTIVE')
-    brands = Brand.objects.filter(status='ACTIVE')
     return {
-        'categories': categories, 'brands': brands
+        'categories': categories,
     }
