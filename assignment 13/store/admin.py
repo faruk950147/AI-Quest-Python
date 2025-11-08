fd2d16b17a3ead10b03d440256f90be2ec4b1977
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-08 15:29:27 +0600

    I have added Rest API

--- assignment 13/store/admin.py
+++ assignment 13/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from store.models import (
+    Category,
+    Brand,
+    Product,
+    Slider
+)
 # Register your models here.
--- assignment 13/store/views.py
+++ assignment 13/store/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from store.models import CategoryType, Category, Brand, Product, Slider
+from django.db.models import Prefetch
+
 # Create your views here.
 class HomeView(generic.View):
+    """
+    # 1. Case-insensitive substring search 
+    # Example: "Pant", "pant", "PANT" — all will match
+    products = Product.objects.filter(category__title__icontains='pant')
+
+    # 2. Case-sensitive substring search
+    # Example: only "pant" will match; "PANT" or "Pant" will not
+    products = Product.objects.filter(category__title__contains='pant')
+
+    # 3. Case-insensitive exact match 
+    # Example: "pant", "PANT", "Pant" — all will match, 
+    # but "pants" or "pant123" will not
+    products = Product.objects.filter(category__title__iexact='pant')
+
+    # 4. Multiple match using 'in' lookup
+    # This returns all products whose category title is either 
+    # 'pant', 'shirt', or 'howdy'
+    products = Product.objects.filter(category__title__in=['pant', 'shirt', 'howdy'])        
+    shirts = Product.objects.filter(category__title__in="SHIRT", status='ACTIVE').select_related('category')
+    """
     def get(self, request):
-        return render(request, 'store/home.html')
+        
+        context = {
+        }
+        return render(request, "store/home.html", context)
+
+class SingleProductView(generic.View):
+    def get(self, request, slug, id):
+        product = get_object_or_404(Product, slug=slug, id=id),
+        context = {
+            'product': product,
+        }
+        return render(request, "store/single-product.html", context)
+    
+class CategoryProductView(generic.View):
+    def get(self, request, slug, id):
+        context = {
+        }
+        return render(request, "store/category-product.html", context)
