b5f42f450a748b88677c305b0207c9f3bc514925
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-13 17:33:54 +0600

    I have added Rest API

--- assignment 29/config/settings.py
+++ assignment 29/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account.apps.AccountConfig'
+    'account.apps.AccountConfig',
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
---
+++ assignment 29/store/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import generic
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+# Create your views here.
+
+@method_decorator(never_cache, name='dispatch')
+class HomeView(generic.View):
+    def get(self, request):
+        return render(request, 'store/home.html')
