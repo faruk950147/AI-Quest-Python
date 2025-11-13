a00ac11f98427ae272f539c2446d4359d6b0363c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-13 16:25:04 +0600

    I have added Rest API

--- assignment 29/account/views.py
+++ assignment 29/account/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views import generic
 from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
@@ -7,6 +8,8 @@ from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 import json
 import re
+
+from account.mixing import LoginRequiredMixin, LogoutRequiredMixin
 
 User = get_user_model()
 
@@ -32,7 +35,6 @@ class UsernameValidationView(generic.View):
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
 
-
 # EmailValidationView
 @method_decorator(never_cache, name='dispatch')
 class EmailValidationView(generic.View):
@@ -56,7 +58,6 @@ class EmailValidationView(generic.View):
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
 
-
 # PasswordValidationView
 @method_decorator(never_cache, name='dispatch')
 class PasswordValidationView(generic.View):
@@ -65,14 +66,78 @@ class PasswordValidationView(generic.View):
             data = json.loads(request.body)
             password = data.get('password', '').strip()
             password2 = data.get('password2', '').strip()
-
+            
             if password != password2:
                 return JsonResponse({'password_error': 'Passwords do not match!'}, status=400)
-
+            
             if len(password) < 8:
                 return JsonResponse({'password_error': 'Your password must be at least 8 characters long.'}, status=400)
 
-            return JsonResponse({'password_valid': True})
+            return JsonResponse({'password_valid': True} , status=200)
 
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+
+# SignInValidationView
+@method_decorator(never_cache, name='dispatch')
+class SignInValidationView(generic.View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            username = data.get('username', '').strip()
+            if not User.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).exists():
+                return JsonResponse({'username_error': 'No account found with this username or email. Please try again.'}, status=404)
+            return JsonResponse({'username_valid': True}, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=404)
+
+# SignUpView
+@method_decorator(never_cache, name='dispatch')
+class SignUpView(LogoutRequiredMixin, generic.View):
+    def get(self, request):
+        return render(request, 'account/sign-up.html')
+    def post(self, request):
+        return render(request, 'account/sign-up.html')
+
+# SignInView     
+@method_decorator(never_cache, name='dispatch')
+class SignInView(LogoutRequiredMixin, generic.View):
+    def get(self, request):
+        
+        return render(request, 'account/sign-in.html')
+    def post(self, request):
+        
+        return render(request, 'account/sign-in.html')
+
+# SignOutView     
+@method_decorator(never_cache, name='dispatch')
+class SignOutView(LogoutRequiredMixin, generic.View):
+    def get(self, request):
+        pass
+    def post(self, request):
+        pass    
+
+# ChangesPasswordView
+@method_decorator(never_cache, name='dispatch')
+class ChangesPasswordView(LoginRequiredMixin, generic.View):
+    def get(self, request):
+        return render(request, 'account/changes-password.html')
+    def post(self, request):
+        return render(request, 'account/changes-password.html')    
+
+# ResetPasswordView
+@method_decorator(never_cache, name='dispatch')
+class ResetPasswordView(LogoutRequiredMixin, generic.View):
+    def get(self, request):
+        return render(request, 'account/reset-password.htm')
+    def post(self, request):
+        return render(request, 'account/reset-password.htm')  
+    
+# ResetPasswordConfirmView
+@method_decorator(never_cache, name='dispatch')
+class ResetPasswordConfirmView(LoginRequiredMixin, generic.View):
+    def get(self, request):
+        return render(request, 'account/reset-password.htm')
+    def post(self, request):
+        return render(request, 'account/reset-password.htm')
+    
