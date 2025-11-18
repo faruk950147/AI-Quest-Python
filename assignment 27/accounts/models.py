c3ef1b2890d5d66f6d9c599f43fc7f093c5398da
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-18 13:20:23 +0600

    I have added Rest API

--- assignment 27/accounts/models.py
+++ assignment 27/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.html import mark_safe
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
 
 DIVISION_CHOICES = (
