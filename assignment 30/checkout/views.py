c5a2d6b6049313abe17436e9553d077e4aea3e8b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-18 13:45:25 +0600

    I have added Rest API

--- assignment 30/checkout/views.py
+++ assignment 30/checkout/views.py
@@ -3,12 +3,21 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.views import generic
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.db.models import Sum, F
 User = get_user_model()
 from accounts.models import Profile
-# Create your views here.
+from cart.models import Cart
 @method_decorator(never_cache, name='dispatch')
 class CheckoutView(generic.View):
+    login_url = reverse_lazy('sign-in')
     def get(self, request):
+        cart_items = Cart.objects.filter(user=request.user, paid=False).select_related("product")
+        summary = cart_items.aggregate(total_price=Sum(F("quantity") * F("product__sale_price")))
+        cart_total = float(summary["total_price"] or 0)
+        shipping_cost = 50
+        grand_total = cart_total + shipping_cost
+        
         return render(request, 'checkout/checkout.html')
     
 @method_decorator(never_cache, name='dispatch')
