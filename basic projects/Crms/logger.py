5259f134f0427501e3b08aceccc2944e2fece404
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-10 09:34:46 +0600

    I have added php

--- basic projects/Crms/logger.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-import logging
-logger = logging.getLogger(__name__)
-
-def test_log(request):
-    logger.debug("Debug Message from View")
-    logger.info("Info Message from View")
-    logger.error("Error Message from View")
-    return HttpResponse("Log successfully written.")
--- basic projects/Crms/tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from tasks.views import (
-    TasksView,
-)
-urlpatterns = [
-    path('', TasksView.as_view(), name='taskview'),
-]
