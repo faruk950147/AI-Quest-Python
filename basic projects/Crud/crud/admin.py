2a9c12ca5f4ea5b2fe87984e06e7c304fd5bbea6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-10 16:28:43 +0600

    I have added php

--- basic projects/Crud/crud/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from crud.models import Student
-
-# Register your models here.
-class StudentAdmin(admin.ModelAdmin):
-    list_display = [
-        'id', 'name', 'department', 'phone'
-    ]
-admin.site.register(Student, StudentAdmin)
