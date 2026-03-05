a3faba67855f6f28bb11c16b0aa63600492988dd
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-05 19:36:45 +0600

    I have some changes

--- assignment 12/CRUD OPERATIONS/using_modal ajax/task/urls.py
+++ assignment 12/CRUD OPERATIONS/using_modal ajax/task/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from task.views import (
-    HomeView, EditedView, DeletedView
+    HomeView, EditedView, IsCompleteView, DeletedView
 )
 
 urlpatterns = [
     path("", HomeView.as_view(), name="HomeView"),
     path("EditedView/<int:id>", EditedView.as_view(), name="EditedView"),
+    path("IsCompleteView/<int:id>", IsCompleteView.as_view(), name="IsCompleteView"),
     path("DeletedView/<int:id>", DeletedView.as_view(), name="DeletedView"),
 ]
