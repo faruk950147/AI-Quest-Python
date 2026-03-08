c5d8e0d9e9597994c4ca092d902f7b37c77a05fc
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-08 09:13:13 +0600

    I have some changes

--- assignment 12/Middleware/home/middleware.py
+++ assignment 12/Middleware/home/middleware.py
@@ -10,3 +10,14 @@ def first_middleware(get_response):
         return response
 
     return first_func
+
+
+class BaseMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        print("this is before view")
+        response = self.get_response(request)
+        print("this is after view")
+        return response
