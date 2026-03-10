ba2f604ce34661568aae71ad7edda4c4b45b14bf
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-10 19:16:34 +0600

    I have some changes

--- assignment 12/querysets/home/models.py
+++ assignment 12/querysets/home/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
-    products = Manager()
+    objects = Manager()
 """
 class Teacher(models.Model):
     name = models.CharField(max_length=200)
