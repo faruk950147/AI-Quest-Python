ed01d9f37b3915ec2700b7d8ed815abc19ed3f6e
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-11 10:39:48 +0600

    I have some changes

--- assignment 12/CRUD OPERATIONS/spa_crud/task/views.py
+++ assignment 12/CRUD OPERATIONS/spa_crud/task/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
+from task.forms import TaskForm
+
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home.html')
+        form = TaskForm()
+        return render(request, 'home.html', {'form': form})
+    
+    def post(self, request):
+        return HttpResponse("POST request")
