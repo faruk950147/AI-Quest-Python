1ba224f3faf97c65ce406ccaeb009703f425c343
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-11 10:27:19 +0600

    I have some changes

--- assignment 12/CRUD OPERATIONS/spa_crud/task/urls.py
+++ assignment 12/CRUD OPERATIONS/spa_crud/task/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from task.views import HomeView, EditedView, IsCompleteView, DeletedView
+from task.views import HomeView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='HomeView'),
-    path('edit/<int:id>/', EditedView.as_view(), name='EditedView'),
-    path('delete/<int:id>/', DeletedView.as_view(), name='DeletedView'),
-    path('iscomplete/<int:id>/', IsCompleteView.as_view(), name='IsCompleteView'),
+    path('', HomeView.as_view(), name='home'),
 ]
