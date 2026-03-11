c2cae6d736f0c75323d49975c02fe63a26e336c8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-11 12:15:08 +0600

    I have some changes

--- assignment 12/CRUD OPERATIONS/spa_crud/task/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from task.models import Task
-
-# Register your models here.
-class TaskAdmin(admin.ModelAdmin):
-    list_display = [
-        'id', 'name', 'department', 'phone', 'is_completed'
-    ]
-admin.site.register(Task, TaskAdmin)
--- assignment 12/CRUD OPERATIONS/spa_crud/task/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from task.views import HomeView, ToggleTaskView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('toggle-task/', ToggleTaskView.as_view(), name='toggle-task'),
-]
