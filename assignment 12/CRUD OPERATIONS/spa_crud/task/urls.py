ef86deccad05209deb796f25884ed13c53234ba1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-11 11:22:53 +0600

    I have some changes

--- assignment 12/CRUD OPERATIONS/spa_crud/task/urls.py
+++ assignment 12/CRUD OPERATIONS/spa_crud/task/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from task.views import HomeView
+from task.views import HomeView, ToggleTaskView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('toggle-task/<int:pk>/', ToggleTaskView.as_view(), name='toggle-task'),
 ]
--- assignment 12/CRUD OPERATIONS/spa_crud/task/views.py
+++ assignment 12/CRUD OPERATIONS/spa_crud/task/views.py
@@ -1,13 +1,41 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import JsonResponse
 
+from task.models import Task
 from task.forms import TaskForm
 
 class HomeView(View):
     def get(self, request):
+        tasks = Task.objects.all()
         form = TaskForm()
-        return render(request, 'home.html', {'form': form})
+        return render(request, 'home.html', {'form': form, 'tasks': tasks})
     
     def post(self, request):
-        return HttpResponse("POST request")
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.is_completed = False
+            task.save()
+            return JsonResponse({
+                'success': True,
+                'task': {
+                    'id': task.id,
+                    'name': task.name,
+                    'department': task.department,
+                    'phone': task.phone,
+                    'is_completed': task.is_completed
+                }
+            })
+        return JsonResponse({'success': False, 'errors': form.errors})
+
+
+class ToggleTaskView(View):
+    def post(self, request, pk):
+        task = Task.objects.get(id=pk)
+        task.is_completed = not task.is_completed
+        task.save()
+        return JsonResponse({
+            'id': task.id,
+            'is_completed': task.is_completed
+        })
