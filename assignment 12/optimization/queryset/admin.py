ccf97249e3c79df49a246a9bd779b8a0c86e9bab
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-19 05:34:11 +0600

    I have some changes

--- assignment 12/optimization/queryset/admin.py
+++ assignment 12/optimization/queryset/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from queryset.models import Author, Post
+from queryset.models import Author, Tag, Post
 # Register your models here.
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'email', 'created_at', 'updated_at']
 admin.site.register(Author, AuthorAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+admin.site.register(Tag, TagAdmin)
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
--- assignment 12/optimization/queryset/models.py
+++ assignment 12/optimization/queryset/models.py
@@ -11,9 +11,27 @@ class Author(models.Model):
         ordering = ["-created_at"]
         verbose_name = "Author"
         verbose_name_plural = "Authors"
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "tag"
+        ordering = ["name"]
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+        
+    def __str__(self):
+        return self.name
         
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -24,3 +42,6 @@ class Post(models.Model):
         ordering = ["-created_at"]
         verbose_name = "Post"
         verbose_name_plural = "Posts"
+        
+    def __str__(self):
+        return self.title
