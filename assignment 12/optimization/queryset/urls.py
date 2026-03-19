7d6364028b79437501b83939f21bb422ceebbbdf
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-19 07:16:26 +0600

    I have some changes

--- assignment 12/optimization/queryset/urls.py
+++ assignment 12/optimization/queryset/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from queryset.views import PostListView
+from queryset.views import PostListView, PostListViewOptimazed
+
 urlpatterns = [
     path('', PostListView.as_view(), name = 'PostListView'),
+    path('optimazed/', PostListViewOptimazed.as_view(), name = 'PostListViewOptimazed'),
 ]
--- assignment 12/optimization/queryset/views.py
+++ assignment 12/optimization/queryset/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views import View
 from queryset.models import Author, Post
 from django.db.models import Count, Avg, Max, Min, Sum, Q, F
+from django.db import connection
 
 
 class PostListView(View):
+    '''Without Optimization select_related'''
     def get(self, request):
-        context = {}
-        return render(request, "home/home.html", context)
+        posts = Post.objects.all()
+        for post in posts:
+            print(post.author.name)
+        for query in connection.queries:
+            print(query['sql'])
+        print(f"Total Number of queries: {len(connection.queries)}")
+        return HttpResponse("Hello World")
 
-    def post(self, request):
+class PostListViewOptimazed(View):
+    '''With Optimization select_related'''
+    def get(self, request):
+        posts = Post.objects.select_related('author')
+        for post in posts:
+            print(post.author.name)
+        for query in connection.queries:
+            print(query['sql'])
+        print(f"Total Number of queries: {len(connection.queries)}")
+        return HttpResponse("Hello World")
 
-        context = {}
-        return render(request, 'home/home.html', context)
