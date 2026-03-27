f57d0ecd354f00a2860aa5168b9546334d85bec9
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-27 11:21:39 +0600

    I have some changes

--- assignment 20/office/consumers.py
+++ assignment 20/office/consumers.py
@@ -1,43 +1,86 @@
 from channels.consumer import SyncConsumer, AsyncConsumer
-
+import time
+import asyncio
+'''
 class OfficeSyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
         self.send({
             'type': 'websocket.accept'
         })
-        print(f'WebSocket connected{self.scope["path"]}')
     
     def websocket_receive(self, event):
         self.send({
             'type': 'websocket.send',
             'text': event['text']
         })
-        for i in range(5):
-            print(f'WebSocket received: {event["text"]}')
     
     def websocket_disconnect(self, event):
         self.send({
             'type': 'websocket.close'
         })
-        print(f'WebSocket disconnected  {self.scope["path"]}')
 
 class OfficeAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         await self.send({
             'type': 'websocket.accept'
         })
-        print(f'WebSocket connected{self.scope["path"]}')
     
     async def websocket_receive(self, event):
         await self.send({
             'type': 'websocket.send',
             'text': event['text']
         })
-        for i in range(5):
-            print(f'WebSocket received: {event["text"]}')
     
     async def websocket_disconnect(self, event):
         await self.send({
             'type': 'websocket.close'
         })
-        print(f'WebSocket disconnected  {self.scope["path"]}')
+        
+
+#  real time data streaming
+class OfficeSyncConsumer(SyncConsumer):
+    def websocket_connect(self, event):
+        self.send({
+            'type': 'websocket.accept'
+        })
+    
+    def websocket_receive(self, event):
+        print(f'WebSocket received: {event["text"]}')
+        for i in range(50):
+            self.send({
+                'type': 'websocket.send',
+                'text': f'Message {i+1}'
+            })
+            time.sleep(1)
+    
+    def websocket_disconnect(self, event):
+        self.send({
+            'type': 'websocket.close'
+        })
+
+
+class OfficeAsyncConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        await self.send({
+            'type': 'websocket.accept'
+        })
+    
+    async def websocket_receive(self, event):
+        print(f'WebSocket received: {event["text"]}')
+        for i in range(5):
+            await self.send({
+                'type': 'websocket.send',
+                'text': f'Message {i+1}'
+            })
+            await asyncio.sleep(1)
+    
+    async def websocket_disconnect(self, event):
+        await self.send({
+            'type': 'websocket.close'
+        })
+'''        
+ 
+ 
+        
+        
+        
