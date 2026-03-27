96bffb5f7df799763a498bd4dea2d227c3bfc7fa
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-27 10:58:52 +0600

    I have some changes

--- assignment 20/config/settings.py
+++ assignment 20/config/settings.py
@@ -130,11 +130,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # WebSocket configuration
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             'hosts': [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
--- assignment 20/office/consumers.py
+++ assignment 20/office/consumers.py
@@ -5,31 +5,39 @@ class OfficeSyncConsumer(SyncConsumer):
         self.send({
             'type': 'websocket.accept'
         })
+        print(f'WebSocket connected{self.scope["path"]}')
     
     def websocket_receive(self, event):
         self.send({
             'type': 'websocket.send',
             'text': event['text']
         })
+        for i in range(5):
+            print(f'WebSocket received: {event["text"]}')
     
     def websocket_disconnect(self, event):
         self.send({
             'type': 'websocket.close'
         })
+        print(f'WebSocket disconnected  {self.scope["path"]}')
 
 class OfficeAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         await self.send({
             'type': 'websocket.accept'
         })
+        print(f'WebSocket connected{self.scope["path"]}')
     
     async def websocket_receive(self, event):
         await self.send({
             'type': 'websocket.send',
             'text': event['text']
         })
+        for i in range(5):
+            print(f'WebSocket received: {event["text"]}')
     
     async def websocket_disconnect(self, event):
         await self.send({
             'type': 'websocket.close'
         })
+        print(f'WebSocket disconnected  {self.scope["path"]}')
