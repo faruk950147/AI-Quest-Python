6ca59ac297acad467fba3d69b5bf9210316b80c1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-28 21:27:23 +0600

    I have some changes

--- assignment 21/chat/routing.py
+++ assignment 21/chat/routing.py
@@ -4,3 +4,4 @@ from chat.consumers import ChatConsumer
 websocket_urlpatterns = [
     path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi()),
 ]
+# ws://localhost:8000/ws/chat/room1/
--- assignment 21/static/main.js
+++ assignment 21/static/main.js
@@ -4,10 +4,25 @@ const sendBtn = document.getElementById("send-btn");
 
 const roomName = "room1"; // dynamic room
 const socket = new WebSocket(`ws://${window.location.host}/ws/chat/${roomName}/`);
+
+// connection status
+socket.onopen = function (e) {
+    console.log("WebSocket connected", e);
+};
+
 // Receive message
 socket.onmessage = function (e) {
-    const data = JSON.parse(e.data);
-    chatBox.value += (data.username || "Anonymous") + ": " + data.message + "\n";
+    console.log("Message received:", e.data);
+};
+
+// connection error
+socket.onerror = function (error) {
+    console.log("WebSocket error:", error);
+};
+
+// connection close
+socket.onclose = function () {
+    console.log("WebSocket closed");
 };
 
 // Send message
